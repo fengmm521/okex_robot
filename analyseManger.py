57d2f2c3dbae17e992e384c44768ffe2363b6805
woodcol <fengmm521@gmail.com>
2018-08-21 22:59:18 +0800

    fix bug

--- analyseManger.py
+++ analyseManger.py
@@ -1238,6 +1238,7 @@ class TradeTool(object):
 
     #交易下单返回状态
     def onOkexTradeBack(self,datadic):
+        isSayOpen = True
         if 'type' in datadic:
             if datadic['type'] == 'pong':
                 self.socketstate['ot'] = True
@@ -1249,8 +1250,8 @@ class TradeTool(object):
                     #okex开多失败，要求重新开多,刚在1秒后重新发送开多
                     msg = {'type':'ol','amount':datadic['data']['amount'],'price':self.okexDatas[1][0]+5,'islimit':1,'cid':datadic['cid']}
                     self.oCIDData[datadic['cid']] = {'msg':msg,'state':0,'cid':datadic['cid']}
-                    
-                    # sayMsg('okex开多失败,1秒后重开多')
+                    if isSayOpen:
+                        sayMsg('okex开多失败,1秒后重开多')
                     print(datadic)
                     time.sleep(1)
                     self.tradeState = 211 #211.okex开多正在下单
@@ -1271,7 +1272,8 @@ class TradeTool(object):
                     #'{"type":"trade","state":"erro","orderType":"%s","amount":%s,"price":%s}'%(outtype,amount,price)
                     msg = {'type':'cl','amount':datadic['data']['amount'],'price':self.okexDatas[0][0]-5,'islimit':1,'cid':datadic['cid']}
                     self.oCIDData[datadic['cid']] = {'msg':msg,'state':0,'cid':datadic['cid']}
-                    # sayMsg('okex平多失败,1秒后重平多')
+                    if isSayOpen:
+                        sayMsg('okex平多失败,1秒后重平多')
                     print(datadic)
                     time.sleep(1) #延时1秒后重新下单
                     self.tradeState = 231 #231.okex平多正在下单
@@ -1289,7 +1291,8 @@ class TradeTool(object):
                     #'{"type":"trade","state":"erro","orderType":"%s","amount":%s,"price":%s}'%(outtype,amount,price)
                     msg = {'type':'os','amount':ptype['amount'],'price':self.okexDatas[0][0]-5,'islimit':1,'cid':datadic['cid']}
                     self.oCIDData[datadic['cid']] = {'msg':msg,'state':0,'cid':datadic['cid']}
-                    sayMsg('okex开空失败,1秒后重开空')
+                    if isSayOpen:
+                        sayMsg('okex开空失败,1秒后重开空')
                     print(datadic)
                     time.sleep(1)
                     self.tradeState = 221 #221.okex开空正在下单
@@ -1307,8 +1310,8 @@ class TradeTool(object):
                     #'{"type":"trade","state":"erro","orderType":"%s","amount":%s,"price":%s}'%(outtype,amount,price)
                     msg = {'type':'cs','amount':datadic['data']['amount'],'price':self.okexDatas[1][0]+5,'islimit':1,'cid':datadic['cid']}
                     self.oCIDData[datadic['cid']] = {'msg':msg,'state':0,'cid':datadic['cid']}
-                    
-                    sayMsg('okex平空失败,1秒后重平空')
+                    if isSayOpen:
+                        sayMsg('okex平空失败,1秒后重平空')
                     print(datadic)
                     time.sleep(1)
                     self.tradeState = 241 #241.okex平空正在下单
